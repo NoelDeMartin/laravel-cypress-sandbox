2d683abaf0784dc22bedb01cf01f464144a39e7b
Noel De Martin <noeldemartin@gmail.com>
2020-08-29 11:00:20 +0200

    Implement decoupled frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <Users />
+</template>
+
+<script>
+import Users from './components/Users.vue';
+
+export default {
+    name: 'App',
+    components: {
+        Users
+    },
+};
+</script>
---
+++ frontend/src/components/Users.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>Users</h1>
+    <ul>
+        <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'Users',
+    data: () => ({ users: [] }),
+    async created() {
+        const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/users`);
+
+        this.users = await response.json();
+    },
+};
+</script>
+
+<style scoped>
+    ul {
+        list-style: none;
+        padding: 0;
+    }
+</style>
